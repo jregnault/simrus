80004f94844baab927b491cc98612ee5079e227c
Jérémie Regnault <jeremie.regnault@outlook.com>
2021-02-03 12:10:55 +0100

    Add window init
    initialize pygame to open a window

--- simrus/simrus.py
+++ simrus/simrus.py
@@ -1,2 +1,20 @@
+import sys
+import pygame
+
+
 if __name__ == "__main__":
-    print("Hello There.")
+    print("Simrus 0.1.0")
+    pygame.display.init()
+
+    size = width, height = 1024, 768
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Simrus")
+
+    keep_running = True
+    while keep_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                keep_running = False
+
+    pygame.display.quit()
+    sys.exit()
